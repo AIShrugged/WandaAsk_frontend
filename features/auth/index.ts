66a00e251cb76fa949d4d9ad4fc3bc618509ee1c
slavapopov <vyacheslav.popov@fabit.ru>
2026-02-18 15:26:16 +0500

    fix build

--- features/auth/index.ts
+++ features/auth/index.ts
@@ -1,6 +1,3 @@
 export { login, register } from './api/auth';
 export { clearSession, logout } from '@/shared/api/session';
 export { AUTH_TITLE_VARIANT, BUTTON_TEXT } from './lib/options';
-export { default as AuthTitle } from './ui/auth-title';
-export { default as LoginForm } from './ui/login-form';
-export { default as RegisterForm } from './ui/register-form';
